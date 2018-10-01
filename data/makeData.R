f57c96e1c40d1f977fb581a9571d5139a6d9ee62
Feng <afeng@urban.org>
2018-10-01 16:33:05 -0400

    Add data and data generation script

---
+++ data/makeData.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(readxl)
+
+name_mapping <- read_excel("Indicator_name_mapping.xlsx", sheet = "mapping")
+labels <- read_excel("source/DC equity text spreadsheet.xlsx")
+
+data_sheets <- excel_sheets("source/Updated data for equity feature.xlsx")
+
+city_dat <- read_excel("source/Updated data for equity feature.xlsx", sheet = data_sheets[1])
+ward_dat <- read_excel("source/Updated data for equity feature.xlsx", sheet = data_sheets[2])
+cluster_dat <- read_excel("source/Updated data for equity feature.xlsx", sheet = data_sheets[3])
+
+dat <- bind_rows(city_dat, ward_dat, cluster_dat) %>%
+  select(-year) %>%
+  left_join(name_mapping, by = c("indicator" = "data_name")) %>%
+  left_join(labels, by=c("text_name" = "Full Indicator label")) %>%
+  select(indicator = text_name, indicator_short = `Abberviated label for menu`,
+         year = `Year`, geo, numerator, denom, value = equityvariable,
+         blue_bar_label = `Blue bar label`, diff_bar_label = `Yellow/pink bar label`,
+         grey_bar_label = `Gray bar label`, summary_sentence = `Summary sentence`)
+
+# add a row with data to initialize the bar chart with
+dat <- add_row(dat, indicator = "Initial",
+                    indicator_short = "Initial",
+                    year = "",
+                    geo = "Initial",
+                    numerator = "",
+                    denom = "",
+                    value = 0,
+                    blue_bar_label = "",
+                    diff_bar_label = "",
+                    grey_bar_label = "",
+                    summary_sentence = NA)
+
+write_csv(dat, "equity_data.csv")
