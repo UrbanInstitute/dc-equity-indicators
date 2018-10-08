99b62a1c9e0f91538671931523239323b79e0cb6
Feng <afeng@urban.org>
2018-10-08 11:43:58 -0400

    Fix incorrect extraction of cluster names in the data

--- data/makeData.R
+++ data/makeData.R
@@ -10,23 +10,24 @@ city_dat <- read_excel("source/Updated data for equity feature_10.5.xlsx", sheet
 ward_dat <- read_excel("source/Updated data for equity feature_10.5.xlsx", sheet = data_sheets[2])
 cluster_dat <- read_excel("source/Updated data for equity feature_10.5.xlsx", sheet = data_sheets[3])
 
-# clean cluster names by getting rid of "Cluster X" from each name
-cluster_dat <- cluster_dat %>%
-  mutate(geo2 = str_split(geo, "\\(")[[1]][2]) %>%
-  mutate(geo3 = str_sub(geo2, end = -2)) %>%
+# clean cluster names by getting rid of "Cluster X" and the parentheses from each name
+# also filter out clusters 42, 45 and 46 which have small sample sizes
+cluster_dat_clean <- cluster_dat %>%
+  filter(!(geo %in% c("Cluster 42 (Observatory Circle)", "Cluster 45 (National Mall, Potomac River)", "Cluster 46 (Arboretum, Anacostia River)"))) %>%
+  mutate(geo2 = str_extract(geo, "\\(.*\\)")) %>%
+  mutate(geo3 = str_sub(geo2, 2, -2)) %>%
   select(indicator, year, "geo" = geo3, numerator, denom, equityvariable)
 
-dat <- bind_rows(city_dat, ward_dat, cluster_dat) %>%
+dat <- bind_rows(city_dat, ward_dat, cluster_dat_clean) %>%
   select(-year) %>%
   left_join(name_mapping, by = c("indicator" = "data_name")) %>%
   left_join(labels, by=c("text_name" = "Full Indicator label")) %>%
   select(indicator = text_name, indicator_short = `Abberviated label for menu`,
          year = `Year`, geo, numerator, denom, value = equityvariable,
          blue_bar_label = `Blue bar label`, diff_bar_label = `Yellow/pink bar label`,
-         grey_bar_label = `Gray bar label`, summary_sentence = `Summary sentence`) %>%
-  filter(!(geo %in% c("Cluster 42 (Observatory Circle)", "Cluster 45 (National Mall, Potomac River)", "Cluster 46 (Arboretum, Anacostia River)")))
+         grey_bar_label = `Gray bar label`, summary_sentence = `Summary sentence`)
 
-# add a row with data to initialize the bar chart with
+# add a row with [fake] data to initialize the bar chart with
 dat <- add_row(dat, indicator = "Initial",
                     indicator_short = "Initial",
                     year = "",
