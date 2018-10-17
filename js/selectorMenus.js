274a68c5350ab8e0926267ad2137e0f4f68ce3c8
Feng <afeng@urban.org>
2018-10-17 13:52:45 -0400

    Disable selecting certain geographies for certain indicators because the sample size is too small

--- js/selectorMenus.js
+++ js/selectorMenus.js
@@ -147,9 +147,27 @@ function showSelectionInMenu(menuElementID, selectorBoxClass) {
     console.log("Base geo:", baseGeo);
     console.log("Comparison geo:", compareGeo);
 
-    // disable link in comparison geography selection modal so that users can't choose to compare a geography against itself
+    d3.selectAll("#baseGeographyMenu .dcEquityIndicators.menuItem").classed("disabled", false);
+    d3.selectAll("#baseGeographyMenu .map .geography").classed("disabled", false);
     d3.selectAll("#comparisonGeographyMenu .dcEquityIndicators.menuItem").classed("disabled", false);
     d3.selectAll("#comparisonGeographyMenu .map .geography").classed("disabled", false);
+
+    // disable Clusters 40 and 43 if indicator is prenatal care and disable clusters 41, 43 and 44 if indicator is affordable housing
+    // due to small sample sizes
+    if(indicator === "Prenatal care") {
+        d3.selectAll("#baseGeographyMenu .cluster_40,.cluster_43").classed("disabled", true);
+        d3.selectAll("#baseGeographyMenu .map .cluster_40,.cluster_43").classed("disabled", true);
+        d3.selectAll("#comparisonGeographyMenu .cluster_40,.cluster_43").classed("disabled", true);
+        d3.selectAll("#comparisonGeographyMenu .map .cluster_40,.cluster_43").classed("disabled", true);
+    }
+    if(indicator === "Affordable home sales") {
+        d3.selectAll("#baseGeographyMenu .cluster_40,.cluster_41,.cluster_43,.cluster_44").classed("disabled", true);
+        d3.selectAll("#baseGeographyMenu .map .cluster_40,.cluster_41,.cluster_43,.cluster_44").classed("disabled", true);
+        d3.selectAll("#comparisonGeographyMenu .cluster_40,.cluster_41,.cluster_43,.cluster_44").classed("disabled", true);
+        d3.selectAll("#comparisonGeographyMenu .map .cluster_40,.cluster_41,.cluster_43,.cluster_44").classed("disabled", true);
+    }
+
+    // disable link in comparison geography selection modal so that users can't choose to compare a geography against itself
     d3.selectAll("#comparisonGeographyMenu .map .cluster_42,.cluster_45,.cluster_46").classed("disabled", true)
     var selectedBaseGeoClassname = d3.select("#baseGeographyMenu .dcEquityIndicators.menuItem.selected").attr("class").split(" ")[2];
     d3.select("#comparisonGeographyMenu .dcEquityIndicators.menuItem." + selectedBaseGeoClassname).classed("disabled", true);
@@ -168,6 +186,8 @@ function showSelectionInMenu(menuElementID, selectorBoxClass) {
     //     d3.select(".comparisonGeographySelector .selectorBox").text("Select a location");
     //     updateEquityBarChart("#equityChart", "Initial", baseGeo, "Initial");
     // }
+
+
 
     // only show graph once an indicator and a base geography have been selected
     indicator !== "Select an indicator" && baseGeo !== "Select a location" && d3.select("#equityChart").classed("initialize", false);
